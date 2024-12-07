06b0331ab9764ea9507bf04c37042db2e4df882b
Luke Sloane-Bulger <luke.sloanebulger@protonmail.com>
2024-12-07 13:02:54 +0000

    frontend

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -7,8 +7,25 @@ import IHabit from '../shared/interfaces/IHabit';
  */
 const Navbar: React.FC = () => {
     const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+    const test = 'debug';
 
-    useEffect(() => sessionStorage.getItem("access-token") ? setIsLoggedIn(true) : setIsLoggedIn(false), [])
+    useEffect(() => sessionStorage.getItem("access-token") || test ? setIsLoggedIn(true) : setIsLoggedIn(false), [])
+
+    const IconButtons = () => {
+      return (
+        <div className="d-flex gap-2">
+          {/* Plus Icon Button */}
+          <button className="btn btn-dark">
+            <i className="bi bi-plus"></i>
+          </button>
+    
+          {/* X Icon Button */}
+          <button className="btn btn-dark">
+            <i className="bi bi-x"></i>
+          </button>
+        </div>
+      );
+    };
 
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
@@ -23,12 +40,7 @@ const Navbar: React.FC = () => {
             <ul className="navbar-nav">
               <li className="nav-item">
 
-                {!isLoggedIn && (
-                  <>
-                    <Link className="navbar-brand text-light" href="/register">Register</Link>
-                    <Link className="navbar-brand text-light" href="/login">Login</Link>
-                  </>
-                )}
+                {isLoggedIn && IconButtons()}
               </li>
             </ul>
           </div>
