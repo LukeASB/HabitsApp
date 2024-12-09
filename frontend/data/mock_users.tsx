815ac5b0a7753e46c161e38729c48c6d0f7d079e
Luke Sloane-Bulger <luke.sloanebulger@protonmail.com>
2024-12-09 17:41:37 +0000

    frontend

---
+++ frontend/data/mock_users.tsx
@@ -0,0 +1,3 @@
+import IRegisterUserRequest from "../shared/interfaces/IRegisterUserRequest";
+
+export const registerUsers: IRegisterUserRequest[] = [{ EmailAddress: "johndoe1@example.com", Password: "secretPassword012!", FirstName: "John", LastName: "Doe"}];
