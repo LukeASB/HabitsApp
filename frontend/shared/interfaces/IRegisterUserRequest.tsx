3e19dc37f6714edf0c1484a832000092b705db87
Luke Sloane-Bulger <luke.sloanebulger@protonmail.com>
2024-12-09 17:40:04 +0000

    frontend

---
+++ frontend/shared/interfaces/IRegisterUserRequest.tsx
@@ -0,0 +1,6 @@
+export default interface IRegisterUserRequest {
+    EmailAddress: string; /*`json:"EmailAddress"`*/
+	Password:     string; /*`json:"Password"`*/
+	FirstName:    string; /*`json:"FirstName"`*/
+	LastName:    string; /*`json:"LastName"`*/
+}
---
+++ frontend/shared/interfaces/IUserRefreshRequest.tsx
@@ -0,0 +1,3 @@
+export default interface UserRefreshRequest {
+	EmailAddress: string; /*`json:"EmailAddress"`*/
+};
